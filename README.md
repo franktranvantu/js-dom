a0eb36d52cd9d770305bce039b113756992e79fd
franktranvantu <59758589+franktranvantu@users.noreply.github.com>
2021-02-17 16:34:04 +0700

    Update

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-Attributes, like the href attribute on a link, or the action attribute on a form, exist as properties of an element object. 
+The properties on the style object represent the various CSS properties we can set. 
 
-You get or set them in a similar way we saw with text in the last section. 
-
-In this section, we'll look at index.html and find an attribute to access.
+This section covers one common use for setting styles with JavaScript: hiding and unhiding elements on the page.
--- final/app.js
+++ final/app.js
@@ -1,9 +1,19 @@
-const input = document.querySelector('input');
+const toggleList = document.querySelector('#toggleList');
+const listDiv = document.querySelector('.list');
+const input = document.querySelector('input.description');
 const p = document.querySelector('p.description');
-const button = document.querySelector('button');
+const button = document.querySelector('button.description');
+
+toggleList.addEventListener('click', () => {
+  if (listDiv.style.display === 'none') {
+    listDiv.style.display = 'block';
+    toggleList.textContent = 'Hide list';
+  } else {
+    listDiv.style.display = 'none';
+    toggleList.textContent = 'Show list';
+  }
+});
 
 button.addEventListener('click', () => {
   p.innerHTML = input.value + ':';
 });
-
-p.title = 'List description';
