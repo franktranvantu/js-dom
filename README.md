149abb5866b0a3f2a9aec47600ae1b9bdf84f208
franktranvantu <59758589+franktranvantu@users.noreply.github.com>
2021-02-18 10:00:55 +0700

    Update

--- README.md
+++
@@ -1,1 +0,0 @@
-This section covers a nextElementSibling.
--- start/app.js
+++ start/app.js
@@ -1,52 +1,9 @@
-const toggleList = document.querySelector('#toggleList');
-const listDiv = document.querySelector('.list');
-const listUl = listDiv.querySelector('ul');
-const descriptionInput = document.querySelector('input.description');
-const descriptionP = document.querySelector('p.description');
-const descriptionButton = document.querySelector('button.description');
-const addItemInput = document.querySelector('.addItemInput');
-const addItemButton = document.querySelector('.addItemButton');
+// The <ul> stored in the variable list has a click event listener that targets each <button> in the list.
+// Complete the code to add a class of highlight to a <p> element that's an immediate previous sibling of the button being clicked.
+var list = document.getElementsByTagName('ul')[0];
 
-listUl.addEventListener('click', (event) => {
-  if (event.target.tagName === 'BUTTON') {
-    if (event.target.className === 'remove') {
-      const button = event.target;
-      const li = button.parentNode;
-      const ul = li.parentNode;
-      ul.removeChild(li);
-    }
+list.addEventListener('click', function(e) {
+  if (e.target.tagName == 'BUTTON') {
 
-    if (event.target.className === 'up') {
-      const button = event.target;
-      const li = button.parentNode;
-      const prevLi = li.previousElementSibling;
-      const ul = li.parentNode;
-      if (prevLi) {
-        ul.insertBefore(li, prevLi);
-      }
-    }
   }
-});
-
-toggleList.addEventListener('click', () => {
-  if (listDiv.style.display === 'none') {
-    listDiv.style.display = 'block';
-    toggleList.textContent = 'Hide list';
-  } else {
-    listDiv.style.display = 'none';
-    toggleList.textContent = 'Show list';
-  }
-});
-
-descriptionButton.addEventListener('click', () => {
-  descriptionP.innerHTML = descriptionInput.value + ':';
-  descriptionInput.value = '';
-});
-
-addItemButton.addEventListener('click', () => {
-  let ul = document.querySelector('ul');
-  let li = document.createElement('li');
-  li.textContent = addItemInput.value;
-  ul.appendChild(li);
-  addItemInput.value = '';
 });
