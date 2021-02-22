549b19cb578779a8fcd935abf021a732c3a7e188
Frank Tran <franktranvantu@gmail.com>
2021-02-22 20:15:31 +0700

    Update

--- start/app.js
+++
@@ -1,7 +0,0 @@
-const myHeading = document.getElementById('myHeading');
-const myTextInput = document.getElementById('myTextInput');
-const myButton = document.getElementById('myButton');
-
-myButton.addEventListener('click', () => {
-  myHeading.style.color = myTextInput.value;
-});
